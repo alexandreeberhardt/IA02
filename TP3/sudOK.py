0fb49112559170854c722881863e881848ce4d40
alexandreeberhardt <alexandre.eber07@gmail.com>
2024-04-18 18:06:11 +0200

    ajout des TP2, TP3 et TP4

---
+++ TP3/sudOK.py
@@ -0,0 +1,250 @@
+"""
+[IA02] TP SAT/Sudoku template python
+author:  Sylvain Lagrue
+version: 1.1.0
+"""
+
+from typing import List, Tuple
+import subprocess
+#import pprint
+from itertools import combinations
+
+# alias de types
+Grid = List[List[int]]
+PropositionnalVariable = int
+Literal = int
+Clause = List[Literal]
+ClauseBase = List[Clause]
+Model = List[Literal]
+
+example: Grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+
+example2: Grid = [
+    [0, 0, 0, 0, 2, 7, 5, 8, 0],
+    [1, 0, 0, 0, 0, 0, 0, 4, 6],
+    [0, 0, 0, 0, 0, 9, 0, 0, 0],
+    [0, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 5, 0, 2, 0],
+    [0, 0, 0, 8, 1, 0, 0, 0, 0],
+    [4, 0, 6, 3, 0, 1, 0, 0, 9],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [7, 2, 0, 0, 0, 0, 3, 1, 0],
+]
+
+
+empty_grid: Grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+
+def write_dimacs_file(dimacs: str, filename: str):
+    "ajoute les dimacs dans un fichier"
+    with open(filename, "w", newline="",encoding="utf8") as cnf:
+        cnf.write(dimacs)
+
+
+def exec_gophersat(
+    filename: str, cmd: str = "gophersat", encoding: str = "utf8"
+) -> Tuple[bool, List[int]]:
+    "execute gophersat en revoie true/false et un modele"
+    result = subprocess.run(
+        [cmd, filename], capture_output=True, check=True, encoding=encoding
+    )
+    string = str(result.stdout)
+    lines = string.splitlines()
+
+    if lines[1] != "s SATISFIABLE":
+        return False, []
+
+    model = lines[2][2:-2].split(" ")
+    modelint = []
+    for i in model:
+        modelint.append(int(i))
+    afficher_grille(model_to_grid(modelint, 9))
+
+    return True, [int(x) for x in model]
+
+
+def cell_to_variable(i: int, j: int, val: int) -> PropositionnalVariable:
+    """i la ligne [0;8], j la colonne [0;8] et val la valeur de la variable"""
+    return 81 * i + 9 * j + val + 1
+
+
+def variable_to_cell(var: PropositionnalVariable) -> Tuple[int, int, int]:
+    "passe d'une variable à une cellue"
+    var -= 1
+    ligne = var // 81
+    col = var % 81 // 9
+    val = var % 9
+    return (ligne, col, val)
+
+
+def model_to_grid(model: Model, nb_vals: int = 9) -> Grid:
+    "passe d'un modele à une grille"
+    sudoku = []
+    for i in range(nb_vals):
+        ligne = []
+        for j in range(nb_vals):
+            for k in range(nb_vals):
+                if model[cell_to_variable(i, j, k - 1)] > 0:
+                    ligne.append(
+                        (model[cell_to_variable(i, j, k - 1)] - 1) % nb_vals + 1
+                    )
+        sudoku.append(ligne)
+    return sudoku
+
+
+def at_least_one(variables: List[PropositionnalVariable]) -> Clause:
+    "ajoute les clauses pour avoir au moins une valeur par case"
+    liste = []
+    for i in variables:
+        liste.append(i)
+    return liste
+
+
+def unique(variables: List[PropositionnalVariable]) -> ClauseBase:
+    "ajoute les clauses pour ne pas avoir plusieurs variables d'une meme liste"
+    clauses = []
+    clauses.append(at_least_one(variables))
+    a = combinations(variables, 2)
+    for item in a:
+        clauses.append([-item[0], -item[1]])
+    return clauses
+
+
+def create_cell_constraints() -> ClauseBase:
+    "ajoute les clauses d'une cellule"
+    clauses = []
+    for i in range(0, 729, 9):
+        clauses += unique(
+            [1 + i, 2 + i, 3 + i, 4 + i, 5 + i, 6 + i, 7 + i, 8 + i, 9 + i]
+        )
+    return clauses
+
+
+def create_line_constraints() -> ClauseBase:
+    "ajoute les clauses d'une ligne"
+    clauses = []
+    for i in range(9):
+        for j in range(9):
+            liste = []
+            for a in range(9):
+                liste.append(cell_to_variable(i, a, j))
+            clauses.append(at_least_one(liste))
+    return clauses
+
+
+def create_column_constraints() -> ClauseBase:
+    "ajoute les clauses d'une colonne"
+    clauses = []
+    for i in range(9):
+        for j in range(9):
+            liste = []
+            for a in range(9):
+                liste.append(cell_to_variable(a, i, j))
+            clauses.append(at_least_one(liste))
+    return clauses
+
+
+def create_box_constraints() -> ClauseBase:
+    "ajout les clauses d'une box"
+    clauses = []
+    for i in range(3):
+        for j in range(3):
+            for z in range(9):
+                liste = []
+                for x in range(3):
+                    for y in range(3):
+                        liste.append((3 * j + y) * 9 + (3 * i + x) * 81 + z + 1)
+                clauses.append(at_least_one(liste))
+
+    return clauses
+
+
+def create_value_constraints(grid: Grid) -> ClauseBase:
+    "ajoute les clauses des valeurs connues de bases "
+    clauses = []
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] != 0:
+                clauses.append([i * 81 + j * 9 + grid[i][j]])
+    return clauses
+
+
+def generate_problem(grid: Grid) -> ClauseBase:
+    "genere les clauses completes à partir d'une grille"
+    clauses = []
+    clauses += create_value_constraints(grid)
+    clauses += create_cell_constraints()
+    clauses += create_line_constraints()
+    clauses += create_column_constraints()
+    clauses += create_box_constraints()
+    return clauses
+
+
+def clauses_to_dimacs(clauses: ClauseBase, nb_vars: int) -> str:
+    "transforme la base de clauses en dimacs"
+    chaine = f"p cnf {nb_vars} {len(clauses)} \n"
+    for i in clauses:
+        for j in i:
+            chaine += f"{j} "
+        chaine += "0\n"
+    return chaine
+
+
+def afficher_grille(grille: Grid):
+    "fonction d'affichage graphique pour que ça soit tout beau"
+    print("-------------------------")
+    for i, ligne in enumerate(grille):
+        if i % 3 == 0 and i != 0:
+            print("-------------------------")
+        print("|", end=" ")
+        for j, valeur in enumerate(ligne):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            print(valeur, end=" ")
+        print("|")
+    print("-------------------------")
+
+
+def main():
+    "main"
+    # print(cell_to_variable(1,3,4))
+    # print(variable_to_cell(cell_to_variable(1,3,4)))
+    # pprint.pp(model_to_grid(model,9))
+    # print(at_least_one([1, 3, 5]))
+    # print(unique([1,3,5]))
+    # print(create_cell_constraints())
+    # print(create_line_constraints())
+    # print(create_column_constraints())
+    # print(cell_to_variable(0,0,0))
+    # print(create_box_constraints())
+    # print(create_value_constraints(example))
+    # print(generate_problem(example))
+    # print (clauses_to_dimacs([[-1, -2], [1, 2], [1, 3], [2, 4], [-3, 4], [-4, 5]],5))
+    # write_dimacs_file(clauses_to_dimacs(generate_problem(example),729),"sudoku.cnf")
+    # afficher_grille(example)
+    exec_gophersat("sudoku.cnf")
+
+
+if __name__ == "__main__":
+    main()
---
+++ TP4/morpion.py
@@ -0,0 +1,193 @@
+
+"""pour le type de strategie et le random.choice() pour jouer au hasard"""
+from typing import Callable
+import random
+
+Grid = tuple[tuple[int, ...], ...]
+State = Grid
+Action = tuple[int, int]
+Player = int
+Score = float
+Strategy = Callable[[Grid, Player], Action]
+
+# Quelques constantes
+DRAW = 0
+EMPTY = 0
+X = 1
+O = 2
+
+
+def grid_tuple_to_grid_list(grid: Grid) -> list[list[int]]:
+    "transforme un tuple en liste"
+    out = []
+    for i in grid:
+        l = []
+        for j in i:
+            l.append(j)
+        out.append(l)
+    return out
+
+
+def grid_list_to_grid_tuple(grid: list[list[int]]) -> Grid:
+    "transforme une liste en tuple"
+
+    out = (
+        (grid[0][0], grid[0][1], grid[0][2]),
+        (grid[1][0], grid[1][1], grid[1][2]),
+        (grid[2][0], grid[2][1], grid[2][2]),
+    )
+    return out
+
+
+def ligne(grid: State, player: Player) -> bool:
+    "dis si une ligne est gagnante"
+
+    for i in range(3):
+        if grid[i][0] == player and grid[i][1] == player and grid[i][2] == player:
+            return True
+    return False
+
+
+def colonne(grid: State, player: Player) -> bool:
+    "dis si une colonne est gagnante"
+
+    for i in range(3):
+        if grid[0][i] == player and grid[1][i] == player and grid[2][i] == player:
+            return True
+    return False
+
+
+def diag(grid: State, player: Player) -> bool:
+    "dis si une diagonale est gagnante"
+    a=grid[0][0] == player and grid[1][1] == player and grid[2][2] == player
+    b=grid[0][2] == player and grid[1][1] == player and grid[2][0] == player
+    return a or b
+
+
+
+def line(grid: State, player: Player) -> bool:
+    "dis si une ligne, colonne ou diagonale est gagnante"
+
+    return ligne(grid, player) or colonne(grid, player) or diag(grid, player)
+
+
+def plein(grid: State) -> bool:
+    "dis si la grille est pleine"
+    for i in range(3):
+        for j in range(3):
+            if grid[i][j] == 0:
+                return False
+    return True
+
+
+def final(grid: State) -> bool:
+    "dis si la partie est terminée"
+    return line(grid, 1) or line(grid, 2) or plein(grid)
+
+
+def score(grid: State) -> float:
+    "donne un score d'une grille : 1 si joueur 1, -1 si joueur 2, 0 si nul"
+    if line(grid, 1):
+        return 1
+    if line(grid, 2):
+        return -1
+    return 0
+
+
+def pprint(grid: State):
+    "affiche proprement une grille"
+    for i in range(3):
+        for j in range(3):
+            if grid[i][j] == 1:
+                print("X", end=" ")
+            elif grid[i][j] == 2:
+                print("O", end=" ")
+            else:
+                print("0", end=" ")
+
+        print("\n")
+
+
+def legals(grid: State) -> list[Action]:
+    "donne les coup légaux"
+    l = []
+    for i in range(3):
+        for j in range(3):
+            if grid[i][j] == 0:
+                l.append((i, j))
+    return l
+
+
+def play(grid: State, player: Player, action: Action) -> State:
+    "fais jouer un joueur"
+    grid = grid_tuple_to_grid_list(grid)
+    grid[action[0]][action[1]] = player
+    return grid_list_to_grid_tuple(grid)
+
+
+def strategy(grid: State, player: Player) -> Action:
+    "permet au joueur humain de jouer"
+    print("à vous de jouer joueur", player)
+    print("ligne : ", end="")
+    l = int(input())
+    print("colonne : ", end="")
+    c = int(input())
+    if (l, c) not in legals(grid):
+        print("coup invalide.")
+        print(grid)
+        print(legals(grid))
+        return strategy(grid, player)
+    return (l, c)
+
+
+def strategy_first_legal(grid: State, player: Player) -> Action:
+    "joue le premier coup légal"
+    player+=1
+    return legals(grid)[0]
+
+
+def strategy_random(grid: State, player: Player) -> Action:
+    "joue un coup au hasard"
+    player+=1
+
+    return random.choice(legals(grid))
+
+
+def tictactoe(strategy_x: Strategy, strategy_o: Strategy, debug: bool = False) -> Score:
+    "joue une partie entre 2 statégies"
+    if not debug:
+        grid = ((0, 0, 0), (0, 0, 0), (0, 0, 0))
+        player = 1
+        while not final(grid):
+            if player == 1:
+                print("-------")
+
+                pprint(grid)
+                grid = play(grid, 1, strategy_x(grid, 1))
+                player = 2
+            else:
+                print("-------")
+                pprint(grid)
+                grid = play(grid, 2, strategy_o(grid, 2))
+                player = 1
+        print("le gagnant est joueur", -score(grid) / 2 + 1.5)
+        pprint(grid)
+        return score(grid)
+    return score(grid)
+
+
+def main():
+    "main"
+    # print(grid_tuple_to_grid_list(((1,2,3),(4,5,6),(7,8,9))))
+    # print(grid_list_to_grid_tuple([[1,2,1],[1,0,1],[2,2,0]]))
+    # grid=[[2,0,1],[1,1,1],[0,2,2]]
+    # pprint(grid)
+    # print(final(grid))
+    # print(legals(grid))
+    # pprint(play(grid,2,strategy(grid,2)))
+    # print(strategy_first_legal(grid,2))
+    tictactoe(strategy, strategy_random)
+
+
+if __name__ == "__main__":
+    main()
